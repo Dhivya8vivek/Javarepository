6f2bb70900abef2d940d0940bf67c397baafb48b
Dhivya <dhivya8vivek@gmail.com>
2024-01-17 11:52:17 +0000

    Second commit for simple java code

--- Addition.java
+++ Addition.java
@@ -6,5 +6,8 @@ System.out.println("Simple Addition");
 int a=10,b=90;
 int result=a+b;
 System.out.println("Result is:"+result);
+System.out.println("Simple Subtraction");
+int result2=a-b;
+System.out.println("Result is:"+result2);
 }
 }
