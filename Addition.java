aa0d62fb8026f31953fb72d84459bc162a8dc492
Dhivya <dhivya8vivek@gmail.com>
2024-01-17 11:32:22 +0000

    First commit for Java class

---
+++ Addition.java
@@ -0,0 +1,10 @@
+public class Addition
+{
+public static void main(String args[])
+{
+System.out.println("Simple Addition");
+int a=10,b=90;
+int result=a+b;
+System.out.println("Result is:"+result);
+}
+}
